b4b461c38fe6846f25eba4d38d83ea522b671e7c
Dimitar Demerdzhiev <ddemerdjiev@gmail.com>
2024-07-26 23:28:59 +0300

    request fixed

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,29 +1,27 @@
 const buildOptions = (data) => {
-    const options = {};
+    const options = {
+        headers: {},
+    };
 
     if (data) {
         options.body = JSON.stringify(data);
-        options.headers = {
-            'content-type': 'application/json'
-        };
+        options.headers['content-type'] = 'application/json';
     }
 
     const token = localStorage.getItem('accessToken');
+
     if (token) {
-        options.headers = {
-            ...options.headers,
-            'X-Authorization': token
-        };
+        options.headers['X-Authorization'] = token; 
     }
 
     return options;
 };
 
 const request = async (method, url, data) => {
-    const options = buildOptions(data);
-    options.method = method;
-
-    const response = await fetch(url, options);
+    const response = await fetch(url, {
+        ...buildOptions(data),
+        method,
+    });
 
     if (response.status === 204) {
         return {};
@@ -43,5 +41,3 @@ export const post = request.bind(null, 'POST');
 export const put = request.bind(null, 'PUT');
 export const remove = request.bind(null, 'DELETE');
 export const patch = request.bind(null, 'PATCH');
-
-
