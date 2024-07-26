b5dcfa5cd774ce1829c5d5ad3c99ac495d1bf9dc
Dimitar Demerdzhiev <ddemerdjiev@gmail.com>
2024-07-27 01:06:42 +0300

    Catalogue page styled

--- client/src/components/service-list/service-list-item/ServiceListItem.jsx
+++ client/src/components/service-list/service-list-item/ServiceListItem.jsx
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import CardBody from "react-bootstrap/esm/CardBody";
+import ListGroup from 'react-bootstrap/ListGroup';
+import CardLink from "react-bootstrap/esm/CardLink";
 
 export default function ServiceListItem({
-    _id,
-    title,
-    price,
-    phoneNumber,
-    imageUrl,
-    description,
+  _id,
+  title,
+  price,
+  phoneNumber,
+  imageUrl,
+  description,
 }) {
-    return (
-        <div className="allServices">
-            <div className="allServices-info">
-                <img src={imageUrl} alt={title} />
-                <h2>{title}</h2>
-                <h3>{price}</h3>
-                <h3>{description}</h3>
-                <h3>{phoneNumber}</h3>
-                <Link to={`/services/${_id}`} className="details-button">Details</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="catalogue">
+      <Card key={_id} className="allServices-info">
+        <Card.Img variant="top" src={imageUrl} alt={title} />
+        <CardBody>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </CardBody>
+        <ListGroup className="list-group-flush">
+        <ListGroup.Item>{price}</ListGroup.Item>
+        <ListGroup.Item>{phoneNumber}</ListGroup.Item>
+      </ListGroup>
+      <CardBody>
+        <Link to={`/services/${_id}` } className="card-link">Details</Link>
+      </CardBody>
+      </Card>
+    </div>
+  );
 }
